a4e305d852016b2e6d6e7f827b9fb55561c9c245
Rasmus Kongsmar Brejl <rasmus@brejl.com>
2015-03-19 17:40:37 +0100

    just a test

---
+++ src/example/test.java
@@ -0,0 +1,16 @@
+package example;
+
+public class test {
+
+	public static void main(String[] args) {
+		int max = 500;
+		int num = 1;
+		
+		for (int i = 1; i<=max;i++){
+			num += i;
+			
+		}
+		System.out.println(num);
+	}
+
+}
