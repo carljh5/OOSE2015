3cfadbea651a2e8a05b4373a21b501ce527c388a
carljh5 <carljh5@gmail.com>
2015-04-15 11:45:26 +0200

    Added collision function(boolean return type) to SpaceObject.

--- src/asteroids/Asteroid.java
+++ src/asteroids/Asteroid.java
@@ -8,7 +8,8 @@ public class Asteroid extends SpaceObject {
 	public Asteroid(int scale) {
 		if(scale < 1)
 			scale = 1;
-		float[] points = {0,0,5,20,30,10,25,-20,10,-12};
+		//float[] points = {0,0,5,20,30,10,25,-20,10,-12};
+		float[] points = {17,0,23,7,37,7,43,20,30,33,30,43,17,40,20,31,00,24};
 		for(float p : points) {
 			p = p*scale;
 		}
