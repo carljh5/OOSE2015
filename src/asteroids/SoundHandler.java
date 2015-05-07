9f1dbba744f8a833ded551121c6531d42f869231
Malthdk <malte@holledig.dk>
2015-05-07 13:02:15 +0200

    Javadoc made for soundhandler class

--- src/asteroids/SoundHandler.java
+++ src/asteroids/SoundHandler.java
@@ -5,12 +5,15 @@ import org.newdawn.slick.Sound;
 
 
 public class SoundHandler {
-	private Sound death;
-	private Sound thrust;
-	private Sound impact;
-	private Sound shoot;
-	private Sound overheat;
 	
+	/**
+	 * Variables for the different sounds
+	 */
+	private Sound death, thrust, impact, shoot, overheat;
+	
+	/**
+	 * Loads all the sounds
+	 */
 	public SoundHandler() {
 		try {
 			death = new Sound("sound/death.ogg");
@@ -23,32 +26,53 @@ public class SoundHandler {
 		}
 	}
 	
+	/**
+	 * Plays sound when player collides
+	 */
 	public void playDeath() {
 		death.play();
 	}
-
+	
+	/**
+	 * Plays sound when player thrusts
+	 */
 	public void playThrust() {
 		if(!thrust.playing())
 		thrust.play();
 	}
 	
+	/**
+	 * Stops thrusting sound
+	 */
 	public void stopThrust() {
 		thrust.stop();
 	}
 	
+	/**
+	 * Plays sound when shot hits asteroid
+	 */
 	public void playImpact() {
 		impact.play();
 	}
 	
+	/**
+	 * Plays sound when shooting
+	 */
 	public void playShoot() {
 		shoot.play();
 	}
 	
+	/**
+	 * Plays sound when overheating
+	 */
 	public void playOverheat() {
 		if(!overheat.playing())
 		overheat.loop();
 	}
 	
+	/**
+	 * Stops sound when overheating
+	 */
 	public void stopOverheat() {
 		overheat.stop();
 	}
