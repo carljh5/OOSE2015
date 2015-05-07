7978e32fe57fc026acdae14d4a864e398791b17c
Malthdk <malte@holledig.dk>
2015-05-07 12:38:52 +0200

    Javadoc for shot

--- src/asteroids/Shot.java
+++ src/asteroids/Shot.java
@@ -2,9 +2,12 @@ package asteroids;
 
 import org.newdawn.slick.geom.Circle;
 
-
 public class Shot extends SpaceObject {
 		
+	/**
+	 * Constructor for generating shots
+	 * @param float x, y and rads: x, y position and size
+	 */
 	Shot(float x, float y, float rads) {
 		shape = new Circle(x,y,1);
 		setSpeed(5.0f);
